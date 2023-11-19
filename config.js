7f882647bb2cbe922bab3576e7c6dd7a1a276fbd
2001gayan <150328996+2001gayan@users.noreply.github.com>
2023-11-19 16:50:22 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['94767939688', sasa', true],
+  ['94767939688', 'sasa', true], 
   [''] 
 ] //Number of owners
 
